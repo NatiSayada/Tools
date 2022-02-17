d60fabbf6209714419e2473058c1461457de6f13
NatiSayada <ssnetanel@gmail.com>
2022-02-17 15:32:04 +0200

    Update win_config.ps1
    update formating

--- ansible/win_config.ps1
+++ ansible/win_config.ps1
@@ -2,10 +2,14 @@
 This script will configure winrm on the server
 #>
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
 $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
 powershell.exe -ExecutionPolicy ByPass -File $file
+
 Set-Item -Path WSMan:\localhost\Service\Auth\Basic -Value $true
 
 Restart-Service WinRM
